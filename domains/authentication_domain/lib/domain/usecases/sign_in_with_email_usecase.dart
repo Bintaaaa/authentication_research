1ab451537cefa18398fd192150eb53141ab80a64
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-05 13:21:19 +0700

    Create sign_in_with_email_usecase.dart

---
+++ domains/authentication_domain/lib/domain/usecases/sign_in_with_email_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:authentication_domain/domain/entities/body/sign_in_with_email_body_entity.dart';
+import 'package:authentication_domain/domain/entities/response/sign_in_response_entity.dart';
+import 'package:authentication_domain/domain/repositories/authentication_repository.dart';
+import 'package:common/utils/response/error/failure_response.dart';
+import 'package:common/utils/usecase/usecase.dart';
+import 'package:dependencies/dartz/dartz.dart';
+
+class SignInUsecase
+    extends UseCase<SignInResponseEntity, SignInWithEmailBodyEntity> {
+  final AuthenticationRepository authenticationRepository;
+
+  const SignInUsecase({
+    required this.authenticationRepository,
+  });
+
+  @override
+  Future<Either<FailureResponse, SignInResponseEntity>> call(
+          SignInWithEmailBodyEntity params) async =>
+      authenticationRepository.sigInWithEmail(
+        signInBodyEntity: params,
+      );
+}
