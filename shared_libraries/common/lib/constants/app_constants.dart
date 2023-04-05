19d9eac18b121eac2c18a026b4a2b0bbddae89a7
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-05 13:21:46 +0700

    Create app_constants.dart

---
+++ shared_libraries/common/lib/constants/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstans {
+  String get emailLabelTextFormField => 'Email Anda';
+  String get passwordLabelTextFormField => 'Kata Sandi';
+  String get nameLabelTextFormField => 'Nama Anda';
+  String get confirmationPasswordLabelTextFormField => 'Konfirmasi Kata Sandi';
+  String get titleAuthSignIn => 'Selamat\nDatang!';
+  String get titleAuthSignUp => 'Mulai Buat\nAkunmu Yuk!';
+  String get titleButtonSignUp => 'Daftar';
+  String get titleButtonSignIn => 'Masuk';
+  String get titleButtonAsGuest => 'Masuk Sebagai Tamu';
+}
