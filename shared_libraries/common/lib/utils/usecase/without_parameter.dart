1c679c10f8006ebd4384ee303d4f607219c31698
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-05 13:22:04 +0700

    Create without_parameter.dart

---
+++ shared_libraries/common/lib/utils/usecase/without_parameter.dart
@@ -0,0 +1,8 @@
+import 'package:dependencies/equatable/equatable.dart';
+
+class WithoutParameter extends Equatable {
+  const WithoutParameter();
+
+  @override
+  List<Object?> get props => [];
+}
