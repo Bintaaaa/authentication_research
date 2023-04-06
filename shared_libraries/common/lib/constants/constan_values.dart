08b4514b32302a88ec08ca980ec5f7a2eae04899
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-06 12:42:29 +0700

    Delete constan_values.dart

--- shared_libraries/common/lib/constants/constan_values.dart
+++
@@ -1,7 +0,0 @@
-import 'package:common/constants/api_constans.dart';
-import 'package:common/constants/app_constants.dart';
-
-class ConstanValues {
-  static AppConstans appConstans = AppConstans();
-  static ApiConstans apiConstans = ApiConstans();
-}
