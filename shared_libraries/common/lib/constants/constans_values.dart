ef925ccf3565bd09af7b8b1bdcbd083fc1febbe1
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-06 12:42:25 +0700

    Create constans_values.dart

---
+++ shared_libraries/common/lib/constants/constans_values.dart
@@ -0,0 +1,7 @@
+import 'package:common/constants/api_constans.dart';
+import 'package:common/constants/app_constants.dart';
+
+class ConstansValues {
+  static AppConstans appConstans = AppConstans();
+  static ApiConstans apiConstans = ApiConstans();
+}
