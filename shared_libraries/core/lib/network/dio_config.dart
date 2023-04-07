dda7b5b275c02e7bf519a9eb713ad64324f2ed53
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-08 04:16:15 +0700

    Update dio_config.dart

--- shared_libraries/core/lib/network/dio_config.dart
+++ shared_libraries/core/lib/network/dio_config.dart
@@ -1,5 +1,7 @@
 import 'package:common/constants/constans_values.dart';
+import 'package:core/network/api_interceptor.dart';
 import 'package:dependencies/dio/dio.dart';
+import 'package:dependencies/flutter_pretty_dio_logger/flutter_pretty_dio_logger.dart';
 
 class DioConfig {
   const DioConfig();
@@ -7,15 +9,22 @@ class DioConfig {
   Dio get dio => _dioConfig();
 
   Dio _dioConfig() {
+    Map<String, dynamic> headers = {};
+    headers['Content-Type'] = 'application/json';
+    headers['x-api-key'] =
+        'RYkvmfnIbJXt2M6pwLoCQ8bhIEaqIAeo7c7eVJIiMNiE1o9bix3ouBS3FvMkTtJF';
+
+    headers['accept'] = 'application/json';
     BaseOptions options = BaseOptions(
       baseUrl: ConstansValues.apiConstans.baseUrl,
-      connectTimeout: 5000,
-      receiveTimeout: 5000,
+      connectTimeout: 20000,
+      receiveTimeout: 20000,
+      headers: headers,
     );
 
     final dio = Dio(options);
 
-    // dio.interceptors.add();
+    dio.interceptors.add(ApiInterceptor.dioLogger());
     return dio;
   }
 }
