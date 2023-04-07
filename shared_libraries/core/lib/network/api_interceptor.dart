581dd1877ed1fcb563128e8f3e08ecab64820205
Bintaaaa <bijantium.yt.32@gmail.com>
2023-04-08 04:16:13 +0700

    Update api_interceptor.dart

--- shared_libraries/core/lib/network/api_interceptor.dart
+++ shared_libraries/core/lib/network/api_interceptor.dart
@@ -1,5 +1,14 @@
+import 'package:dependencies/dio/dio.dart';
 import 'package:dependencies/flutter_pretty_dio_logger/flutter_pretty_dio_logger.dart';
 
-class ApiInterceptor extends PrettyDioLogger {
-  
+class ApiInterceptor {
+  static Interceptor dioLogger() => PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90,
+      );
 }
